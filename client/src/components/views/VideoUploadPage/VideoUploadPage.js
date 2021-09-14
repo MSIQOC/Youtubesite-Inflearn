8e74bf3f10decdb3fe8b56e0b0e334c44f876d21
msiqoc <edison98@naver.com>
2021-09-14 17:32:41 +0900

    비디오 업로드 폼 만들기 2까지 잘됨

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -4,9 +4,38 @@ import Dropzone from 'react-dropzone';
 const { TextArea } = Input;
 const { Title } = Typography;
 
+const PrivateOptions = [
+	{value: 0, label: "Private" },
+	{value: 1, label: "Public"}
+]
+
+const CategoryOptions = [
+	{ value: 0, label: "Film & Animation"},
+	{ value: 1, label: "Autos & Vehicles"},
+	{ value: 2, label: "Music"},
+	{ value: 3, label: "Pets & Animals"},
+]
+
 //functional component
 function VideoUploadPage() {
+	
+	const [VideoTitle, setVideoTitle] = useState("")
+	const [Description, setDescription] = useState("")
+	const [Private, setPrivate] = useState(0)
+	const [Category, setCategory] = useState("Film & Animation")
 
+	const onTitleChange = (e) => {
+		setVideoTitle(e.currentTarget.value)
+	}
+	const onDescriptionChange = (e) => {
+		setDescription(e.currentTarget.value)
+	}
+	const onPrivateChange = (e) => {
+		setPrivate(e.currentTarget.value)
+	}
+	const onCategoryChange = (e) => {
+		setCategory(e.currentTarget.value)
+	}
 	return (
 		<div style={{ maxWidth:'700px', margin:'2rem auto' }}>
 			<div style = {{ textAlign:'center', marginBottom:'2rem' }}>
@@ -40,26 +69,30 @@ function VideoUploadPage() {
 			<br />
 			<label>Title</label>
 			<Input
-				onChange
-				value
+				onChange={onTitleChange}
+				value={VideoTitle}
 			/>
 			<br />
 			<br />
 			<label>Description</label>
 			<TextArea
-				onChange
-				value
+				onChange={onDescriptionChange}
+				value={Description}
 			/>
 			<br />
 			<br />
-
-			<select onChange>
-				<option key value></option>
+			<select onChange={onPrivateChange}>
+				{PrivateOptions.map((item, index) => (
+					<option key={index} value={item.value}>{item.label}</option>
+				))}
 			</select>
 
 			<br />
 			<br />
-			<select onChange>
+			<select onChange={onCategoryChange}>
+				{CategoryOptions.map((item, index) => (
+					<option key={index} value={item.value}>{item.label}</option>
+				))}
 				<option key value></option>
 			</select>
 			<br />
