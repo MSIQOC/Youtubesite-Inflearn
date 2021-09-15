16dc52c03dd604b0a6129abf84b5885f1c3a8ba3
msiqoc <edison98@naver.com>
2021-09-15 21:43:43 +0900

    why

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,13 +1,37 @@
 import React, { useState } from 'react';
 import { Typography, Button, Form, message, Input, Icon } from 'antd';
 import Dropzone from 'react-dropzone';
+<<<<<<< HEAD
 //import Axios from 'axios';
+=======
+>>>>>>> parent of 965cb3b... 클라이언트 안되는 문제 생김
 const { TextArea } = Input;
 const { Title } = Typography;
 
 //functional component
 function VideoUploadPage() {
+<<<<<<< HEAD
 
+=======
+	
+	const [VideoTitle, setVideoTitle] = useState("")
+	const [Description, setDescription] = useState("")
+	const [Private, setPrivate] = useState(0)
+	const [Category, setCategory] = useState("Film & Animation")
+
+	const onTitleChange = (e) => {
+		setVideoTitle(e.currentTarget.value)
+	}
+	const onDescriptionChange = (e) => {
+		setDescription(e.currentTarget.value)
+	}
+	const onPrivateChange = (e) => {
+		setPrivate(e.currentTarget.value)
+	}
+	const onCategoryChange = (e) => {
+		setCategory(e.currentTarget.value)
+	}
+>>>>>>> parent of 965cb3b... 클라이언트 안되는 문제 생김
 	return (
 		<div style={{ maxWidth:'700px', margin:'2rem auto' }}>
 			<div style = {{ textAlign:'center', marginBottom:'2rem' }}>
@@ -19,9 +43,9 @@ function VideoUploadPage() {
 
 					{/* Drop zone */}
 					<Dropzone 
-					onDrop={onDrop}
-					multiple={false}
-					maxSize={1000000000}
+					onDrop
+					multiple
+					maxSize
 					>
 					{({ getRootProps, getInputProps }) => (
 						<div style={{ width: '300px', height: '240px', border:'1px solid lightgray', display:'flex',
