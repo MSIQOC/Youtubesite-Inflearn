965cb3ba2a5fdb1a1f8c9e8ad07c2aa07f85fad3
msiqoc <edison98@naver.com>
2021-09-15 18:45:44 +0900

    클라이언트 안되는 문제 생김

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Typography, Button, Form, message, Input, Icon } from 'antd';
 import Dropzone from 'react-dropzone';
+import Axios from 'axios';
 const { TextArea } = Input;
 const { Title } = Typography;
 
@@ -36,6 +37,24 @@ function VideoUploadPage() {
 	const onCategoryChange = (e) => {
 		setCategory(e.currentTarget.value)
 	}
+	const onDrop = (files) => {
+		let formData = new FormData;
+		const config = {
+			header: {'content-type': 'multipart/form-data'}
+		}
+		formData.append("file",files[0])
+		
+		//서버에 보내기. 서버쪽에서 '/api/video/uploadfiles'에 대한 라우트 만들어야함.
+		Axios.post('/api/video/uploadfiles', formData, config)
+			.then(response => {
+				if(response.data.success) {
+
+				} else{
+					alert('비디오 업로드를 실패했습니다.')
+				}
+			})
+
+	}
 	return (
 		<div style={{ maxWidth:'700px', margin:'2rem auto' }}>
 			<div style = {{ textAlign:'center', marginBottom:'2rem' }}>
@@ -47,9 +66,9 @@ function VideoUploadPage() {
 
 					{/* Drop zone */}
 					<Dropzone 
-					onDrop
-					multiple
-					maxSize
+					onDrop={onDrop}
+					multiple={false}
+					maxSize={1000000000}
 					>
 					{({ getRootProps, getInputProps }) => (
 						<div style={{ width: '300px', height: '240px', border:'1px solid lightgray', display:'flex',
---
+++ server/routes/video.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+//const { Video } = require("../models/Video");
+//const multer = require("multer");
+const { auth } = require("../middleware/auth");
+
+//=================================
+//             Video
+//=================================
+router.post('/uploadfiles', (req, res) => {
+
+})
+
+module.exports = router;
