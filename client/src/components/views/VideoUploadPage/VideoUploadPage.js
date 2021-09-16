e5611f12e9a4976d20e51b4812b5ec505c2d0d20
msiqoc <edison98@naver.com>
2021-09-16 14:47:27 +0900

    클라부분 포트 8000으로 바꿈

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -4,14 +4,28 @@ import Dropzone from 'react-dropzone';
 const { TextArea } = Input;
 const { Title } = Typography;
 
+const PrivateOptions = [
+	{ value: 0, label: "Private" },
+	{ value: 1, label: "Public"}
+]
+const CategoryOptions = [
+	{ value: 0, label: "Film & Animation"},
+	{ value: 1, label: "Autos & Vehicles"},
+	{ value: 2, label: "Music"},
+	{ value: 3, label: "Pets & Animals"},
+]
+
 //functional component
 function VideoUploadPage() {
-	
+
 	const [VideoTitle, setVideoTitle] = useState("")
 	const [Description, setDescription] = useState("")
-	const [Private, setPrivate] = useState(0)
-	const [Category, setCategory] = useState("Film & Animation")
 
+	// 네모 화살표 내려서 선택하는 창 만들기 (0번째 것을 default로 지정해준다.)
+	const [Private, setPrivate] = useState(0)
+
+	// 카테고리는 처음 미리 선택돼있는게 Film & Animation이기 때문에 그렇게 설정해준다.
+	const [Category, setCategory] = useState("Film & Animation")
 	const onTitleChange = (e) => {
 		setVideoTitle(e.currentTarget.value)
 	}
@@ -24,6 +38,7 @@ function VideoUploadPage() {
 	const onCategoryChange = (e) => {
 		setCategory(e.currentTarget.value)
 	}
+
 	return (
 		<div style={{ maxWidth:'700px', margin:'2rem auto' }}>
 			<div style = {{ textAlign:'center', marginBottom:'2rem' }}>
@@ -35,9 +50,9 @@ function VideoUploadPage() {
 
 					{/* Drop zone */}
 					<Dropzone 
-					onDrop
-					multiple
-					maxSize
+						onDrop
+						multiple
+						maxSize
 					>
 					{({ getRootProps, getInputProps }) => (
 						<div style={{ width: '300px', height: '240px', border:'1px solid lightgray', display:'flex',
@@ -57,27 +72,30 @@ function VideoUploadPage() {
 			<br />
 			<label>Title</label>
 			<Input
-				onChange
-				value
+				onChange={onTitleChange}
+				value={VideoTitle}
 			/>
 			<br />
 			<br />
 			<label>Description</label>
 			<TextArea
-				onChange
-				value
+				onChange={onDescriptionChange}
+				value={Description}
 			/>
 			<br />
 			<br />
 
-			<select onChange>
-				<option key value></option>
+			<select onChange={onPrivateChange}>			
+				{PrivateOptions.map((item, index) => (
+					<option key={index} Dvalue = {item.value}>{item.label}</option>
+				))}
 			</select>
-
 			<br />
 			<br />
-			<select onChange>
-				<option key value></option>
+			<select onChange={onCategoryChange}>
+				{CategoryOptions.map((item, index) => (
+					<option key={index} value = {item.value}>{item.label}</option>
+				))}
 			</select>
 			<br />
 			<br />
